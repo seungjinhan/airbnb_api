0e3d05831e4c8d300959ccbea3fdb5de50898c88
hansj <hanblues@gmail.com>
2020-08-26 10:36:35 +0900

    'user'

--- users/serializers.py
+++ users/serializers.py
@@ -2,17 +2,24 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
+        )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = (
-            "groups",
-            "user_permissions",
-            "password",
-            "last_login",
-            "is_superuser",
             "is_staff",
             "is_active",
             "date_joined",
-            "favs",
         )
